3643a536bd11a1655e0694d49bf7e4d5cfdaa6b5
Tank <bluepastrock@gmail.com>
2025-03-18 17:43:25 -0400

    Did the roughdraft for the message page. need someone to make the ui prettier imao.

--- app/(group)/message.jsx
+++ app/(group)/message.jsx
@@ -3,29 +3,35 @@ import { useLocalSearchParams, useRouter } from "expo-router"
 import useAppwrite from "../../lib/useAppwrite"
 import MessageBubble from "../../components/MessageBubble"
 import { getMessages } from "../../lib/appwrite"
-import { View,Text,ActivityIndicator,FlatList } from "react-native"
-import { useState,useEffect } from "react"
+import { View,Text,ActivityIndicator,FlatList,TextInput } from "react-native"
+import { useState,useEffect,useRef } from "react"
 const message = ()=>{
    const {SessionID} = useLocalSearchParams()
    const[isLoading,setIsLoading]= useState(true);
     const {data:messages,refetch}=useAppwrite(()=>getMessages(SessionID))
      const { user, setUser, setIsLogged } = useGlobalContext();
+     const previousUserRef=useRef(null);
      useEffect(()=>{
       if((messages!=null)&&(messages.length>0)&&(isLoading==true))
       {
       setIsLoading(false);
       }
      },[messages])
-return (<View className="flex-1 items-center mt-5">
+return (<View className="flex-1 items-center bg-white">
   {isLoading==true?
-  (<ActivityIndicator size="large" color="#0000ff"/>):
-  (<FlatList
+  (<ActivityIndicator size="large" color="#0000ff"/>
+  ):(
+  <View className="w-[95%] flex-col">
+    <FlatList
     data={messages}
     keyExtractor={(item, index) => index.toString()}
-    renderItem={({ item }) => <MessageBubble message={item} />}
-    className='w-[90%] mt-10'
-          />)
-  }
+    renderItem={({ item }) => {
+      const isConsecutiveMessage = previousUserRef.current?.sender.username==item.sender.username
+      console.log(isConsecutiveMessage);
+      previousUserRef.current=item
+    return <MessageBubble message={item} isConsecutive={isConsecutiveMessage}/>
+  }} className='w-[90%] mt-10'/>
+  </View>)}
   </View>)
 }
 export default message
--- components/MessageBubble.jsx
+++ components/MessageBubble.jsx
@@ -1,12 +1,31 @@
-import { View,Text } from "react-native";
-const MessageBubble=(message)=>{
-const {times,read,receiver,sender,Message,sessionID}= message;
-return(<View className="w-full h-40">
-    <View className="h-full w-[50%] bg-red-500">
-        <View className="w-16 h-16">
-
+import { View,Text,Image } from "react-native";
+const MessageBubble=({message,isConsecutive})=>{
+const {time,read,receiver,sender,Message,sessionID}= message;
+console.log(time);
+const formatedTime = new Date(time).toLocaleDateString("en-US");
+return(<View className="w-full flex-row bg-white">
+{ !isConsecutive ? 
+  (<View className="w-full flex-row mt-4">
+    <View className="w-[40px] h-[40px] rounded-full ml-[10px] mt-2 mb-2 overflow-hidden">
+              <Image source={{ uri: sender.avatar}} className="w-full h-full" />
+    </View>
+    <View className="w-full">
+        <View className=" flex-row">
+            <Text className="ml-5 mt-1 text-[16px] font-pregular">{sender.username}</Text>
+            <Text className="ml-5 mt-1 text-[16px] font-pregular">{formatedTime}</Text>
+        </View>
+        <View className="flex-row w-full">
+            <Text className="ml-5 mt-1 text-[16px] font-pregular">{Message}</Text>  
         </View>
     </View>
+  </View>
+    ):(
+    <View className="flex-row w-full">
+        <View className="w-[50px]">  
+
+        </View>
+        <Text className="ml-5  text-[16px] font-pregular">{Message}</Text>  
+    </View>)}
 </View>)
 }
 export default MessageBubble
